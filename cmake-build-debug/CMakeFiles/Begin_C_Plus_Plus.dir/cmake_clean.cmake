9ec2d88a349b801e30a37f150742919a799b97c3
upulka <upulka.saku@gmail.com>
2023-01-21 22:58:17 +0530

    Initial project creating

---
+++ cmake-build-debug/CMakeFiles/Begin_C_Plus_Plus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Begin_C_Plus_Plus.exe"
+  "Begin_C_Plus_Plus.exe.manifest"
+  "Begin_C_Plus_Plus.pdb"
+  "CMakeFiles/Begin_C_Plus_Plus.dir/main.cpp.obj"
+  "libBegin_C_Plus_Plus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Begin_C_Plus_Plus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
